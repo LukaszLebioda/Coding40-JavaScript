15de34149ebc0d9268e613749dad8b3c8b6da7ac
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-09-15 23:41:00 +0200

    some real js beginnings

---
+++ Bro Code - absolute beginnings/index.js
@@ -0,0 +1,17 @@
+// to output sth on the www page
+console.log("I like pizza!");
+console.log("it's really good!");
+
+// to make a window pop up
+window.alert("Go away");
+
+// so this is a single-line comment
+
+/*
+    and 
+    this 
+    is 
+    a
+    multi-line
+    comment
+*/
