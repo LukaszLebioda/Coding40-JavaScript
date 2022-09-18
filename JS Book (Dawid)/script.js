e57ef8632c692fa6fdd7ed69c0826db4c7c712c0
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-09-18 22:58:34 +0200

    New js script to welcome the User with a proper greeting depending on the time of the day

---
+++ JS Book (Dawid)/script.js
@@ -0,0 +1,33 @@
+// ctrl + / daje nam komentarz
+// shift + alt + strzałka w dół kopiuje linijkę
+// clg = console.log(object);
+
+/* skrypt do wyświetlania Userowi przeglądającemu stronę
+odpowiedniego powitania
+w zależności od czasu systemowego jego komputera */
+
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 23)
+{
+    greeting = "Good Night!";
+}
+else if (hourNow > 19)
+{
+    greeting = "Good Evening!";
+}
+else if (hourNow > 17) 
+{
+    greeting = "Good Afternoon!";
+}
+else if (hourNow > 9)
+{
+    greeting = "Good Morning!";
+}
+else 
+{
+    greeting = "Welcome!"
+}
+document.write("<h3><em>" + greeting + "</em></h3>");
