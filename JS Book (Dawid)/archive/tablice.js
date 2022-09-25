d7a6b4b881684285f07b1ff194fbd9c6e2946b17
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-09-25 19:49:22 +0200

    arrays in java script practice

---
+++ JS Book (Dawid)/archive/tablice.js
@@ -0,0 +1,20 @@
+// ctrl + / daje nam komentarz
+// shift + alt + strzałka w dół kopiuje linijkę
+// clg = console.log(object);
+
+// zadeklarowanie zmiennei i przypisanie do niej listy wartości (tablicy)
+let colors = ["white", "black", "red", "orange", "pink"];
+
+const colorsNumber = colors[1];
+
+// wyświetlenie ilości wartości w tablicy i konkretnej wartości (po indeksie)
+console.log(colors.length);
+console.log(colors[3]);
+console.log(colorsNumber);
+// zmiana wartości poszczególnych pozycji w tablicy
+colors[0] = "snowwhite";
+colors[1] = "nightblack";
+colors[2] = "bloodred";
+colors[3] = "darkorange";
+colors[4] = "girlspink";
+console.log(colors[3]);
--- JS Book (Dawid)/script.js
+++ JS Book (Dawid)/script.js
@@ -2,10 +2,4 @@
 // shift + alt + strzałka w dół kopiuje linijkę
 // clg = console.log(object);
 
-let colors = ["white", "black", "red", "orange", "pink"];
-
-const colorsNumber = colors
-
-document.write(colorsNumber.length);
-
 
