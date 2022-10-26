a25dba6fb54ab9045549289dad3627a1da361479
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-10-26 23:03:45 +0200

    url loading, part of url checking, element h1 cheking

---
+++ Cypress 1/cypress/e2e/url(visit, url, get),.cy.js
@@ -0,0 +1,20 @@
+// first it - loading URL with timeout option (fails when timeout exceeds 10s)
+// second it - checking whether a certain text is a part of an loaded URL 
+// third it - checking, whether an element (e.g. a header h1) is visible on the page
+describe("Browser actions", () => {
+  it("should load correct URL", () => {
+    cy.visit("https://example.com/", { timeout: 10000 })
+  })
+  it("should check correct URL", () => {
+    cy.url().should("include", "example.com" )
+  })
+  it("should check for correct element on the page", () => {
+    cy.get("h1").should("be.visible")
+  })
+})
+
+
+
+
+
+
