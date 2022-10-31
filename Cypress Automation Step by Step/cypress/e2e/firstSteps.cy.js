6c0aef8926ce94c4a11c3cf5072033ea62c7bf88
LukaszLebioda <lukasz.leb@gmail.com>
2022-10-31 10:54:55 +0100

    new Cypress course

---
+++ Cypress Automation Step by Step/cypress/e2e/firstSteps.cy.js
@@ -0,0 +1,11 @@
+// describe('empty spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
+// how to open www pages:
+it("Google Search", () => {
+
+  cy.visit("https://google.com")
+})
