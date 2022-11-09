2a28badcde4f0f76befbdd126e640bbcbee5b397
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-11-09 21:34:48 +0100

    needed to reinstall cypress, just cheking sth

---
+++ Cypress/Cypress Udemy/cypress/e2e/newtest.cy.js
@@ -0,0 +1,24 @@
+it("should load the page", () => {
+    cy.visit("https://books.toscrape.com/catalogue/olio_984/index.html", {timeout: 10000})
+})
+
+it("should check book price", () => {
+  cy.get
+})
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
