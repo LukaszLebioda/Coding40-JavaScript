d9e73648d34b312fd332c8ce51bb24a60f9cd85c
Łukasz Lebioda <lukasz.leb@gmail.com>
2022-11-28 22:50:39 +0100

    new program installation: fuelcounter

--- JavaScript/BroCode/script.js
+++ JavaScript/BroCode/script.js
@@ -1,11 +1,26 @@
-// array.reduce() - reduces an array to a single valu (e.g. sums up all the elements - like in the online store where items being purchased are summed up in the cart)
+// // array.reduce() - reduces an array to a single valu (e.g. sums up all the elements - like in the online store where items being purchased are summed up in the cart)
 
-let prices = [5, 10, 12, 19, 28, 30, 100];
-let total = prices.reduce(checkOut);
+// let prices = [5, 10, 12, 19, 28, 30, 100];
+// let total = prices.reduce(checkOut);
 
-console.log(total);
+// console.log(total);
 
-function checkOut(total, item) {
-    return total + item
-}
-
+// function checkOut(total, item) {
+//     return total + item
+// }
+// NUMBER CONSTRUCTOR / STRING CONSTRUCTOR / BOOLEAN CONSTRUCTOR
+let age = window.prompt("How old are You?");
+age += 1
+console.log(age);
+// expected ouptut, e.g. 401
+let age2 = window.prompt("How old are You???");
+age2 = Number(age2)
+age2 += 1
+console.log(age2);
+// expected output: eg. 41
+let y = String(3.14)
+console.log(y, typeof y); 
+// expected output: 3.14 string
+let z;
+z = Boolean("dsfsfgfg");
+console.log(z, typeof z);
--- JavaScript/Udemy JS/script.js
+++ JavaScript/Udemy JS/script.js
@@ -1,41 +1,41 @@
-// pobieranie elementów na stronie
+// // pobieranie elementów na stronie
 
-console.log("---------stare sposoby------------");
+// console.log("---------stare sposoby------------");
 
-// stare sposoby:
-const test = document.getElementById("item");
-console.log(test);
+// // stare sposoby:
+// const test = document.getElementById("item");
+// console.log(test);
 
-const test2 = document.getElementsByTagName("h1");
-console.log(test2);
+// const test2 = document.getElementsByTagName("h1");
+// console.log(test2);
 
-const test3 = document.getElementsByClassName("listItem");
-console.log(test3);
+// const test3 = document.getElementsByClassName("listItem");
+// console.log(test3);
 
-console.log("-------nowe sposoby--------------");
+// console.log("-------nowe sposoby--------------");
 
-// nowe sposoby:
+// // nowe sposoby:
 
-// znajduje pierwszy element li na stronie
-const test4 = document.querySelector("ul"); 
-console.log(test4);
-// znajduje pierwszy element o id item na stronie (używamy # albo . )
-// jako argument podajemy zapisy jak w css
-const test5 = document.querySelector("#item"); 
-console.log(test5);
-// można szukać w zmiennych
-const ulList = document.querySelector("ul");
-const liItem = ulList.querySelector("li");
-console.log(liItem);
+// // znajduje pierwszy element li na stronie
+// const test4 = document.querySelector("ul"); 
+// console.log(test4);
+// // znajduje pierwszy element o id item na stronie (używamy # albo . )
+// // jako argument podajemy zapisy jak w css
+// const test5 = document.querySelector("#item"); 
+// console.log(test5);
+// // można szukać w zmiennych
+// const ulList = document.querySelector("ul");
+// const liItem = ulList.querySelector("li");
+// console.log(liItem);
 
-// znajduje wszystkie elementy
-const test6 = document.querySelectorAll("h1"); // ("h1.class") or (".class")
-console.log(test6);
+// // znajduje wszystkie elementy
+// const test6 = document.querySelectorAll("h1"); // ("h1.class") or (".class")
+// console.log(test6);
 
-console.log("-------żywe kolekcje---------");
+// console.log("-------żywe kolekcje---------");
 
-// żywe kolekcje
-const newLi = document.createElement("li");
-ulList.appendChild(newLi).textContent = "qwerty";
-console.log(ulList.length);
+// // żywe kolekcje
+// const newLi = document.createElement("li");
+// ulList.appendChild(newLi).textContent = "qwerty";
+// console.log(ulList.length);
 
