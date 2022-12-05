9b7854b025bd4767825996f6e0c5650ccb64a865
LukaszLebioda <lukasz.leb@gmail.com>
2022-12-05 19:01:51 +0100

    separated cypress from js practice

--- Cypress/Cypress Automation/cypress/e2e/a visit, get.cy.js
+++
@@ -1,8 +0,0 @@
-
-// grabs a searchbox, types something inside, then clicks the search button or presses enter
-it("Google Search", () => {
-  cy.visit("https://www.google.pl/");
-  cy.get('.gLFyf').type("Automation Step by Step {Enter}");
-  
-  // alternatywa: cy.contains('Szukaj w Google').click()
-})
--- Cypress/Cypress Udemy/cypress/e2e/autosport.cy.js
+++
@@ -1,8 +0,0 @@
-describe("I want to check whether there are any new articles about my favourite WRC rally driver", () => {
-  it("should load the page", () => {
-    cy.visit("https://us.motorsport.com/", {timeout: 10000})
-  })
-  it("should check URL", () => {
-    cy.url().should("include", "us")
-  })
-})
